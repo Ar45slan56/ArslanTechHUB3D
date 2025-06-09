7e32a38d5cacc5273f8c0e9132e35275ed21ca3b
Ar45slan56 <arman40591@gmail.com>
2025-06-10 01:28:13 +0500

    hero mation update anon

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,9 +1,43 @@
+const words = [
+  { text:Ideas, imgPath: '/img/ideas.svg' },
+  { text:Concepts, imgPath: '/img/concepts.svg' },
+]
+
 const Hero = () => {
   return (
-    <h1 className="text-3xl underline">
-      Hero section
-    </h1>
-  )
-}
+    <section id="hero" className="relative overflow-hidden">
+      <div className="absolute top-0 left-0 z-10">
+        <img src="/images/bg.png" alt="background image is Loading...." />
+      </div>
 
-export default Hero
+<div className="hero_layout">
+{/* left : hero content */}
+<header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5  ">
+<div className="flex flexx-col gap-7">
+<div className="hero-text">
+  <h1>Shaping 
+    <span className="slide">
+      <span className="wrapper">
+        {words.map((word) => (
+          <span key={word.text}></span>
+         
+        ))}
+      </span>
+    </span>
+  </h1>
+  <h1>Tomorrow’s Technology</h1>
+  <h1>Through Today’s Innovation</h1>
+</div>
+
+
+</div>
+
+</header>
+</div>
+
+
+    </section>
+  );
+};
+
+export default Hero;
